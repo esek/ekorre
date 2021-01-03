07b59b063c84118fabf8735ce16901b6968d7e3c
blennster <emil.blennow99@gmail.com>
2021-01-03 19:45:43 +0100

    Fixat sql

--- src/sql/access.sqlite.sql
+++ src/sql/access.sqlite.sql
@@ -3,16 +3,17 @@ BEGIN TRANSACTION;
 CREATE TABLE "PostAccess" (
 	"ref"	TEXT,
 	"resourcetype"	TEXT,
-	"resouce"	TEXT,
-	PRIMARY KEY("refusername","resouce"),
-	FOREIGN KEY("refusername") REFERENCES "Users"("username")
+	"resource"	TEXT,
+	PRIMARY KEY("ref","resouce"),
+	FOREIGN KEY("ref") REFERENCES "Posts"("postname")
 );
 
 CREATE TABLE "IndividualAccess" (
 	"ref"	TEXT,
 	"resourcetype"	TEXT,
 	"resource"	TEXT,
-	PRIMARY KEY("refusername","resource")
-)
+	PRIMARY KEY("ref","resource")
+	FOREIGN KEY("ref") REFERENCES "Users"("username")
+);
 
 END TRANSACTION;
