74f7c6e42e1b3056cacee7a5018aa2b4ffd7b887
blennster <emil.blennow99@gmail.com>
2021-01-03 19:46:05 +0100

    Litet stavfel

--- src/util.ts
+++ src/util.ts
@@ -7,5 +7,5 @@
 export const dependecyGuard = (name: string, dependecies: string[]): void => {
   const modules = JSON.parse(process.env.MODULES ?? '[]') as string[];
   if (!dependecies.every((e) => modules.includes(e)))
-    throw new Error(`Modules ${dependecies.join(' ')} is required by ${name}!`);
+    throw new Error(`Module(s) ${dependecies.join(' ')} is required by ${name}!`);
 };
