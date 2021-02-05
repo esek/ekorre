4505147892fc1eb919fb1132a0cd9e12bfe19179
Emil Jonathan Eriksson <eje1999@gmail.com>
2021-02-05 17:18:07 +0100

    Lägger till saker i API
    Inte riktigt klar med hur de mer avancerade sakerna ska fixas än. Har
    lagom bra koll på hur allt ska sättas ihop.

--- src/api/article.api.ts
+++ src/api/article.api.ts
@@ -13,25 +13,46 @@ export class articleAPI {
   /**
    * Hämta alla artiklar
    */
-  async getArticles(): Promise<any> {
-    return null;
+  async getAllArticles(): Promise<Article[]> {
+    const allArticles = await knex<Article>(ARTICLE_TABLE);
+
+    return allArticles;
+  }
+
+  /** 
+   * Hämtar alla nyhetsartiklar
+  */
+  async getAllNewsArticles(): Promise<Article[]> {
+    const allNewsArticles = await knex<Article>(ARTICLE_TABLE).where('articleType', 'news');
+
+    return allNewsArticles;
+  }
+
+  async getAllInformationArticles(): Promise<Article[]> {
+    const allInformationArticles = await knex<Article>(ARTICLE_TABLE).where('articleType', 'information');
+
+    return allInformationArticles;
   }
 
   /**
    * Hämta en specifik artikel efter ID
    * @param id artikel-id:n
    */
-  async getArticle(id: ID): Promise<any> {
-    return null;
+  async getArticle(id: string): Promise<Article | null> {
+    const article = await knex<Article>(ARTICLE_TABLE).where('id', id).first();
+
+    return article ?? null;
   }
-  // Gör så att man kan välja efter creator, datum, tags etc.
+  // TODO: Gör så att man kan välja efter creator, datum, tags etc.
 
   /**
    * Hämtar de senaste nyhetsartiklarna
    * @param nbr antal artiklar
    */
-  async getLatestNews(nbr: Number): Promise<any> {
-    return null;
+  async getLatestNews(limit: number): Promise<Article[]> {
+    const lastestNews = await knex<Article>(ARTICLE_TABLE).where('articleType', 'news').orderBy('createdat', 'desc').limit(limit);
+    
+    return lastestNews;
   }
 
   /**
@@ -47,7 +68,7 @@ export class articleAPI {
    * modifieras via API:n
    * @param entry Modifiering av existerande artikel
    */
-  async modifyArticle(entry: ModifyArticle): Primise<any> {
+  async modifyArticle(entry: ModifyArticle): Promise<any> {
     return null;
   }
 }
