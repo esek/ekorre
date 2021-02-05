cbfa045229fe48a4e386dd57a92b7b0f122a44cb
Emil Jonathan Eriksson <eje1999@gmail.com>
2021-02-05 20:19:37 +0100

    Lägger till anti-XSS
    Okej är lite i valet och kvalet. TEKNISKT sätt bör en obehörig aldrig
    kunna lägga till nyheter, men med tanke på att standardlösenordet är för
    svagt är det nog en god idé att bara rensa ut script-taggar ur artiklar.
    
    Har svårt att se användningsområden för det, och skulle man behöva kan
    man ju se till att sanitera själv.

--- src/reducers/article.reducer.ts
+++ src/reducers/article.reducer.ts
@@ -9,8 +9,24 @@ const converter = new showdown.Converter(SHOWDOWN_CONVERTER_OPTIONS);
  * @param md string formatted as Markdown
  */
 export function convertMarkdownToHtml(md: string): string {
-  const html: string = converter.makeHtml(md);
+  let html: string = converter.makeHtml(md);
+  html = removeScriptTags(html);
   return html;
+}
+
+/**
+ * Motverkar XSS, främst för att script-taggar inte borde få vara med.
+ * @param html html med okända script-taggar
+ */
+function removeScriptTags(html : string): string {
+  var div = document.createElement('div');
+  div.innerHTML = html;
+  var scripts = div.getElementsByTagName('script');
+  var i = scripts.length;
+  while (i--) {
+    scripts[i].parentNode?.removeChild(scripts[i]);  // Frågetecknet ser till att vi inte anropar på null
+  }
+  return div.innerHTML;
 }
 
 /**
