a04c7441a3f4ba6e3264911564b40118207e8088
Emil Jonathan Eriksson <eje1999@gmail.com>
2021-02-09 20:27:43 +0100

    En version av article-API:n BORDE fungera nu, men fett osäkert
    Jag tryckte in lite utropstecken, men det är nog en rätt dålig idé. Vad
    returnerar man om man faktiskt inte hittar något? Who knows...

--- src/reducers/article.reducer.ts
+++ src/reducers/article.reducer.ts
@@ -1,26 +1,19 @@
 import { showdown } from 'showdown';
 import { sanitize } from 'dompurify';
 import { SHOWDOWN_CONVERTER_OPTIONS } from './constants';
-import { ArticleAPI, ArticleModel } from '../api/article.api';
+import { Resolvers } from '../graphql.generated'
+import { ArticleModel } from '../api/article.api';
 import type { Article, User } from '../graphql.generated';
 
 const converter = new showdown.Converter(SHOWDOWN_CONVERTER_OPTIONS);
 
-function articleReduce(article: ArticleModel, markdown: boolean): Article {
-  // Hur ska man hantera detta? Vi vill ju inte skicka med allt iaf....
-  const creator: User = {
-    // Använder refuser från ArticleModel för att få detta?
-  };
+function articleReduce(article: ArticleModel, markdown: boolean): ArticleModel {
 
   // Vi lagrar alltid HTML i databasen; vi gör om till markdown vid
   // förfrågan
   article.body = markdown ? article.body : convertHtmlToMarkdown(article.body);
 
-  // Strip sensitive data! https://stackoverflow.com/a/50840024
-  // eslint-disable-next-line @typescript-eslint/no-unused-vars
-  const { refuser, ...reduced } = article;
-  const a = { creator, ...reduced };
-  return a;
+  return article;
 }
 
 /**
@@ -43,13 +36,14 @@ function convertHtmlToMarkdown(html: string): string {
 }
 
 // Vi definierar Reducers för alla olika typer av ArticleModels
-export async function articleReducer(a: ArticleModel, markdown: boolean): Promise<Article>;
-export async function articleReducer(a: ArticleModel[], markdown: boolean): Promise<Article>;
-export async function articleReducer(a: ArticleModel | ArticleModel[], markdown: boolean): Promise<Article | Article[]> {
+// Vi returnerar ArticleModel; refuser -> User i resolvern
+export async function articleReducer(a: ArticleModel, markdown: boolean): Promise<ArticleModel>;
+export async function articleReducer(a: ArticleModel[], markdown: boolean): Promise<ArticleModel>;
+export async function articleReducer(a: ArticleModel | ArticleModel[], markdown: boolean): Promise<ArticleModel | ArticleModel[]> {
   // Är det en array, reducera varje för sig, annars skicka bara tillbaka en reducerad
   if (a instanceof Array) {
     const aa = await Promise.all(a.map((e) => articleReduce(e, markdown)));
     return aa;
   }
-  return articleReduce(a);
+  return articleReduce(a, markdown);
 }
--- src/resolvers/article.resolver.ts
+++ src/resolvers/article.resolver.ts
@@ -1,8 +1,11 @@
 import { Article, NewArticle, ModifyArticle, ArticleType, Resolvers } from '../graphql.generated';
 import { ArticleAPI } from '../api/article.api';
+import { UserAPI } from '../api/user.api';
+import { userReducer } from '../reducers/user.reducer';
 import { articleReducer } from '../reducers/article.reducer'
 
 const articleApi = new ArticleAPI();
+const userApi = new UserAPI();
 
 const articleResolver: Resolvers = {
   Query: {
@@ -21,9 +24,14 @@ const articleResolver: Resolvers = {
       };
       return u;
     },
-    article: (_, { id, markdown }) => {
+    article: async (_, { id, markdown }) => {
       markdown = markdown ?? false;  // If markdown not passed, returns default (false)
-      return articleReducer(await articleApi.getArticle(id), markdown);
+      // Vi får tillbaka en ArticleModel som inte har en hel användare, bara unikt användarnamn.
+      // Vi måste använda UserAPI:n för att få fram denna användare.
+      const articleModel = await articleReducer((await articleApi.getArticle(id!))!, markdown);
+      const creator = await userReducer((await userApi.getSingleUser(articleModel.refuser))!);
+      const { refuser, ...reduced } = articleModel;
+      return { creator, ...reduced };
     },
     test: () => new Date(),
   },
