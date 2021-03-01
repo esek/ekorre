ad11802d28846cf7b3bcf27253fb6f2a84d8db2c
Emil Jonathan Eriksson <eje1999@gmail.com>
2021-03-01 21:36:32 +0100

    Fixar skit i articleReduce

--- src/reducers/article.reducer.ts
+++ src/reducers/article.reducer.ts
@@ -1,25 +1,17 @@
 import { showdown } from 'showdown';
 import { sanitize } from 'dompurify';
 import { SHOWDOWN_CONVERTER_OPTIONS } from './constants';
-import { Resolvers } from '../graphql.generated'
+import { Resolvers } from '../graphql.generated';
 import { ArticleModel } from '../api/article.api';
 import type { Article, User } from '../graphql.generated';
 
 const converter = new showdown.Converter(SHOWDOWN_CONVERTER_OPTIONS);
 
-function articleReduce(article: ArticleModel, markdown: boolean): ArticleModel {
-  // Vi lagrar alltid HTML i databasen; vi gör om till markdown vid
-  // förfrågan
-  article.body = markdown ? article.body : convertHtmlToMarkdown(article.body);
-
-  return article;
-}
-
 /**
  * Converts MarkDown to HTML
  * @param md string formatted as Markdown
  */
-function convertMarkdownToHtml(md: string): string {
+export function convertMarkdownToHtml(md: string): string {
   let html = converter.makeHtml(md);
   html = sanitize(html);  // Don't want any dirty XSS xD
   return html;
@@ -32,6 +24,17 @@ function convertMarkdownToHtml(md: string): string {
 function convertHtmlToMarkdown(html: string): string {
   const md = converter.makeMarkdown(html);
   return md;
+}
+
+function articleReduce(article: ArticleModel, markdown: boolean): ArticleModel {
+  // Vi lagrar alltid HTML i databasen; vi gör om till markdown vid
+  // förfrågan
+  const sanatizedBody = markdown ? article.body : convertHtmlToMarkdown(article.body);
+  // eslint-disable-next-line @typescript-eslint/no-unused-vars
+  const { body, ...reduced } = article;
+  const a = {...reduced, body: sanatizedBody };
+
+  return a;
 }
 
 // Vi definierar Reducers för alla olika typer av ArticleModels
