647a631a70c94f335ef5978b394cd133b83dd9d9
Emil Blennow <emil.blennow99@gmail.com>
2021-04-19 18:05:47 +0200

    Ny token varje dag

--- src/auth.ts
+++ src/auth.ts
@@ -1,8 +1,22 @@
 import jwt from 'jsonwebtoken';
+import crypto from 'crypto';
 
 import { Logger } from './logger';
 
-const SECRET = 'bigone';
+let currentSecret: string;
+let secretDate: Date;
+
+/**
+ * Generera en ny secret varje ny dag.
+ */
+const SECRET = () => {
+  const now = new Date();
+  if (currentSecret == null || secretDate.getDay() !== new Date().getDay()) {
+    currentSecret = crypto.randomBytes(20).toString('hex');
+    secretDate = now;
+  }
+  return currentSecret;
+};
 const EXPIRE_MINUTES = 10;
 const logger = Logger.getLogger('Auth');
 const invalidTokenStore: Set<string> = new Set();
@@ -28,7 +42,7 @@ const checkTokenStore = (token: string): boolean => {
  */
 export const verifyToken = <T>(token: string): T => {
   if (checkTokenStore(token)) throw Error('JWT token is in blacklist!');
-  const u = jwt.verify(token, SECRET);
+  const u = jwt.verify(token, SECRET());
   logger.debug(`Authorized a token with value: ${Logger.pretty(u)}`);
   return u as unknown as T; // Kan bli problem senare...
 };
@@ -38,7 +52,7 @@ export const verifyToken = <T>(token: string): T => {
  * @param o - Ett objekt
  */
 export const issueToken = (o: Record<string, unknown>): string => {
-  const token = jwt.sign(o, SECRET, { expiresIn: `${EXPIRE_MINUTES}min` });
+  const token = jwt.sign(o, SECRET(), { expiresIn: `${EXPIRE_MINUTES}min` });
   logger.debug(`Issued a token for object: ${Logger.pretty(o)}`);
   return token;
 };
