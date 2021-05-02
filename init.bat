758a959ff516e24231a570b3c40d204636e54097
Emil Blennow <emil.blennow99@gmail.com>
2021-05-02 16:24:11 +0200

    Uppdaterat init skript som löser #24

--- init.bat
+++ init.bat
@@ -1,11 +1,17 @@
 @echo off
 set DATABASE_PATH=sqlite_database.db
+set PUBLIC_PATH=%CD%\public.local
 
 sqlite3 --version >nul 2>&1 && (
   echo "Skapar sqlite3 databas under namnet %DATABASE_PATH%"
   sqlite3 -init src/sql/init.sql $DATABASE_PATH .exit
   copy .env.example.dev .env
   echo "DB_FILE=%DATABASE_PATH%" >> .env
+
+  echo "Skapar lokal public mapp under namnet %PUBLIC_PATH%"
+  mkdir %PUBLIC_PATH%
+  xcopy /E public %PUBLIC_PATH%
+  echo "FILE_ROOT=%PUBLIC_PATH%" >> .env
 
   echo "Installerar alla npm paket"
   npm install
