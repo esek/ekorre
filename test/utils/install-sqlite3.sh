9ff3ff16b55b4191265115ada4ace8ea2b1306f2
Emil Jonathan Eriksson <eje1999@gmail.com>
2021-09-01 22:48:24 +0200

    WWWWWWWWWWWWWWWWWWWWWWWWWWWW

--- test/utils/install-sqlite3.sh
+++ test/utils/install-sqlite3.sh
@@ -3,9 +3,9 @@
 echo "Installing sqlite3..."
 
 if [ -n "$(command -v yum)" ]; then
-  sudo dnf install sqlite3 -y
+  dnf install sqlite3 -y
 elif [ -n "$(command -v apt)" ]; then
-  sudo apt install sqlite3 -y
+  apt install sqlite3 -y
 else
   echo "Failed to install sqlite3"
   exit 1
