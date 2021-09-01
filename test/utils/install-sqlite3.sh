8486c0174d69fb7cfa5052b8154e28cb6d560ae9
Emil Jonathan Eriksson <eje1999@gmail.com>
2021-09-01 22:08:55 +0200

    Trying to fix this shit

---
+++ test/utils/install-sqlite3.sh
@@ -0,0 +1,14 @@
+#! /bin/bash
+
+echo "Installing sqlite3..."
+
+if [ -n command -v yum ]; then
+  dnf install sqlite3 -y
+elif [ -n command -v apt ]; then
+  apt install sqlite3 -y
+else
+  echo "Failed to install sqlite3"
+  exit 1
+fi
+
+exit 0
