79b8bab7074fab80bf77bcfa7e7b548546ffee41
Emil Jonathan Eriksson <eje1999@gmail.com>
2021-09-02 11:19:02 +0200

    Mmmmmmmmm marabou

--- test/testvariables.ts
+++ test/testvariables.ts
@@ -1,3 +1,19 @@
+import { readFile } from 'fs';
+import { join } from 'path';
+
+let port: string | undefined;
+
+readFile(join(__dirname, '../.env'), 'utf8', (err, data) => {
+  if (err) {
+    throw new Error(err.toString());
+  } else {
+    const match = RegExp('(?<=PORT=)(d+)').exec(data);
+    if (match) {
+      [port] = match;
+    }
+  }
+});
+
 // Vi antar att en test-dev-server kör på localhost
-const PORT = process.env.PORT ?? (() => { throw new Error('Port for test dev server not defined'); })();
-export const API_URL = `http://localhost:${PORT}`;
+port = port ?? (() => { throw new Error('Port for test dev server not defined'); })();
+export const API_URL = `http://localhost:${port}`;
