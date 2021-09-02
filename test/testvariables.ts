dc3dcda3e443fbd25057ebbb2a7327c66618fb8f
Emil Jonathan Eriksson <eje1999@gmail.com>
2021-09-02 11:42:58 +0200

    GUTEFÅR, LAMMKÖTT, BÄÄÄÄÄÄÄÄÄÄ

--- test/testvariables.ts
+++ test/testvariables.ts
@@ -1,18 +1,16 @@
-import { readFile } from 'fs';
+import { readFileSync } from 'fs';
 import { join } from 'path';
 
 let port: string | undefined;
 
-readFile(join(__dirname, '../.env'), 'utf8', (err, data) => {
-  if (err) {
-    throw new Error(err.toString());
-  } else {
-    const match = RegExp('(?<=PORT=)(d+)').exec(data);
-    if (match) {
-      [port] = match;
-    }
+const config = readFileSync(join(__dirname, '../.env'), 'utf8');
+
+((data) => {
+  const match = /(?<=PORT=)(\d+)/g.exec(data);
+  if (match) {
+    [port] = match;
   }
-});
+})(config);
 
 // Vi antar att en test-dev-server kör på localhost
 port = port ?? (() => { throw new Error('Port for test dev server not defined'); })();
