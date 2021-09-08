229a7c89b6e795db85e40e98d50b8af663db5ed2
Emil Jonathan Eriksson <eje1999@gmail.com>
2021-09-08 09:57:10 +0200

    Added jest coverage

--- jest.config.js
+++ jest.config.js
@@ -2,5 +2,11 @@
 module.exports = {
   preset: 'ts-jest/presets/default',
   testEnvironment: 'node',
+  collectCoverage: true,
+  // Alla JS, JSX, TS, TSX-filer i src, men inte models
+  collectCoverageFrom: [
+    'src/**/*.{js,jsx,ts,tsx}',
+    '!src/**/*.d.ts'
+  ],
   setupFiles: ["dotenv/config"], // Så jest kommer åt .env
 };
