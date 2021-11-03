83417ecee6a954cca811beb1ab2599ef0ead9957
Emil Jonathan Eriksson <eje1999@gmail.com>
2021-11-03 14:04:43 +0100

    Cheeky random testObj

--- test/unit/auth.test.ts
+++ test/unit/auth.test.ts
@@ -6,15 +6,23 @@ import type { StrictObject } from '../../src/models/base';
 
 interface TestType extends StrictObject {
   test: string;
-  daddy: string;
-  please: string;
 }
 
-const testObj: TestType = {
-  test: 'me',
-  daddy: 'now',
-  please: 'OwO',
-};
+// Creates new random test object, but checks that it
+// has not been created before
+const createRandomTestObj = (() => {
+  const usedValues: string[] = [];
+
+  return (): TestType => {
+    let randomData = randomBytes(20).toString('hex');
+    while (usedValues.includes(randomData)) {
+      randomData = randomBytes(20).toString('hex');
+    }
+
+    usedValues.push(randomData);
+    return { test: randomData } as TestType;
+  };
+})();
 
 // Vi kontrollerar att jwts sign-metod kallas
 const signSpy = jest.spyOn(jwt, 'sign');
@@ -28,6 +36,8 @@ beforeEach(() => {
 });
 
 test('issueToken for accessToken should use jwt to sign', () => {
+  const testObj = createRandomTestObj();
+
   const token = issueToken(testObj, 'accessToken');
 
   expect(token).not.toBeNull();
@@ -35,6 +45,8 @@ test('issueToken for accessToken should use jwt to sign', () => {
 });
 
 test('issueToken for refreshToken should use jwt to sign', () => {
+  const testObj = createRandomTestObj();
+
   const token = issueToken(testObj, 'refreshToken');
 
   expect(token).not.toBeNull();
@@ -42,6 +54,8 @@ test('issueToken for refreshToken should use jwt to sign', () => {
 });
 
 test('creating, verifying and invalidating accessToken', () => {
+  const testObj = createRandomTestObj();
+
   const token = issueToken(testObj, 'accessToken');
   const decodedToken = verifyToken<TestType>(token, 'accessToken');
 
@@ -61,6 +75,9 @@ test('creating, verifying and invalidating accessToken', () => {
 });
 
 test('creating, verifying and invalidating refreshToken', () => {
+  const testObj = createRandomTestObj();
+
+
   const token = issueToken(testObj, 'refreshToken');
   const decodedToken = verifyToken<TestType>(token, 'refreshToken');
 
@@ -80,6 +97,8 @@ test('creating, verifying and invalidating refreshToken', () => {
 });
 
 test('invalidate accessToken after EXPIRE_MINUTES.accessToken minutes', () => {
+  const testObj = createRandomTestObj();
+
   const token = issueToken(testObj, 'accessToken');
 
   // Verifiera att token fungerar
@@ -93,10 +112,12 @@ test('invalidate accessToken after EXPIRE_MINUTES.accessToken minutes', () => {
     .useFakeTimers()
     .setSystemTime(new Date(trueTime.getTime() + EXPIRE_MINUTES.accessToken * 60000));
 
-  expect(() => verifyToken(token, 'refreshToken')).toThrowError();
+  expect(() => verifyToken(token, 'accessToken')).toThrowError();
 });
 
 test('invalidate refreshToken after EXPIRE_MINUTES.refreshToken minutes', () => {
+  const testObj = createRandomTestObj();
+
   const token = issueToken(testObj, 'refreshToken');
 
   // Verifiera att token fungerar
@@ -111,4 +132,30 @@ test('invalidate refreshToken after EXPIRE_MINUTES.refreshToken minutes', () => 
     .setSystemTime(new Date(trueTime.getTime() + EXPIRE_MINUTES.refreshToken * 60000));
 
   expect(() => verifyToken(token, 'refreshToken')).toThrowError();
+});
+
+test('valid accessToken is invalid refreshToken', () => {
+  const testObj = createRandomTestObj();
+
+  const token = issueToken(testObj, 'accessToken');
+
+  // Verifiera att token fungerar
+  const decodedToken = verifyToken<TestType>(token, 'accessToken');
+  const { iat, exp, ...reducedDecodedToken } = decodedToken;
+  expect(reducedDecodedToken).toStrictEqual(testObj);
+
+  expect(() => verifyToken(token, 'refreshToken')).toThrowError();
+});
+
+test('valid refreshToken is invalid accessToken', () => {
+  const testObj = createRandomTestObj();
+
+  const token = issueToken(testObj, 'refreshToken');
+
+  // Verifiera att token fungerar
+  const decodedToken = verifyToken<TestType>(token, 'refreshToken');
+  const { iat, exp, ...reducedDecodedToken } = decodedToken;
+  expect(reducedDecodedToken).toStrictEqual(testObj);
+
+  expect(() => verifyToken(token, 'accessToken')).toThrowError();
 });
