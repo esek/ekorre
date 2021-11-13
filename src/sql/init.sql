5614b61b59b78cc3da40e51ae2bffb732c0e46bf
Emil Jonathan Eriksson <eje1999@gmail.com>
2021-11-13 21:50:59 +0100

    Added more SQL

--- src/sql/init.sql
+++ src/sql/init.sql
@@ -154,4 +154,23 @@ IF NOT EXISTS "PasswordReset"
   "time" NUMBER NOT NULL
 );
 
+CREATE TABLE IF NOT EXISTS "Meetings" (
+  "id" INTEGER PRIMARY KEY AUTOINCREMENT,
+  "type" TEXT NOT NULL,
+  "number" INTEGER,
+  "year" INTEGER NOT NULL,
+  "refsummons" TEXT,
+  "refdocuments" TEXT,
+  "reflateDocuments" TEXT,
+  "refprotocol" TEXT,
+  FOREIGN KEY("refsummons") REFERENCES "Files"("id"),
+  FOREIGN KEY("refdocuments") REFERENCES "Files"("id"),
+  FOREIGN KEY("reflateDocuments") REFERENCES "Files"("id"),
+  FOREIGN KEY("refprotocol") REFERENCES "Files"("id"),
+);
+
+INSERT INTO Meetings (type, year) VALUES ('VM', 2020);
+INSERT INTO Meetings (type, number, year) VALUES ('SM', 1, 1962);
+INSERT INTO Meetings (type, number, year) VALUES ('Extra', 1, 2050);
+
 END TRANSACTION;
--- src/sql/meeting.sqlite.sql
+++ src/sql/meeting.sqlite.sql
@@ -12,3 +12,7 @@ CREATE TABLE IF NOT EXISTS "Meetings" (
   FOREIGN KEY("reflateDocuments") REFERENCES "Files"("id"),
   FOREIGN KEY("refprotocol") REFERENCES "Files"("id"),
 );
+
+INSERT INTO Meetings (type, year) VALUES ('VM', 2020);
+INSERT INTO Meetings (type, number, year) VALUES ('SM', 1, 1962);
+INSERT INTO Meetings (type, number, year) VALUES ('Extra', 1, 2050);
