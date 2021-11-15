1a084fdc69c6edcbfa31cccb0cccdb4d4c26d882
Emil Jonathan Eriksson <eje1999@gmail.com>
2021-11-15 12:02:14 +0100

    Fixes some errors

--- src/sql/init.sql
+++ src/sql/init.sql
@@ -169,7 +169,7 @@ CREATE TABLE IF NOT EXISTS "Meetings" (
   FOREIGN KEY("refprotocol") REFERENCES "Files"("id")
 );
 
-INSERT INTO Meetings (type, year) VALUES ('VM', 2020);
+INSERT INTO Meetings (type, number, year) VALUES ('VM', 1, 2020);
 INSERT INTO Meetings (type, number, year) VALUES ('SM', 1, 1962);
 INSERT INTO Meetings (type, number, year) VALUES ('Extra', 1, 2050);
 
