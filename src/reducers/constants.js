67e2de6557ad39e1446c94e3b8f158efa400f5ad
Emil Jonathan Eriksson <eje1999@gmail.com>
2021-11-16 14:08:12 +0100

    Revert "AAAAAAAAAAAAAAAAAH"
    This reverts commit ecf5e012481a7ce69f46a7a79bdd4c62061e71dc.

--- src/reducers/constants.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-exports.__esModule = true;
-exports.SHOWDOWN_CONVERTER_OPTIONS = void 0;
-// Decides how Markdown <--> HTML conversion is done
-exports.SHOWDOWN_CONVERTER_OPTIONS = {
-    headerLevelStart: 1,
-    emoji: true,
-    noHeaderId: true
-};
