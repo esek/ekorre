457c026b7e597fce6d0acb0e5f87d523daa05569
Emil Jonathan Eriksson <eje1999@gmail.com>
2021-11-16 18:13:33 +0100

    Fixes some errors

--- src/sql/election/electable.sqlite.sql
+++ src/sql/election/electable.sqlite.sql
@@ -1,5 +1,7 @@
 CREATE TABLE IF NOT EXISTS "Electable" (
   "refpost" TEXT NOT NULL,
-  "refelection" TEXT NOT NULL,
+  "refelection" INTEGER NOT NULL,
+  FOREIGN KEY("refpost") REFERENCES "Posts"("postname"),
+  FOREIGN KEY("refelection") REFERENCES "Elections"("id"),
   CONSTRAINT "uniqueElectable" UNIQUE ("refpost", "refelection")
 );
--- src/sql/election/proposal.sqlite.sql
+++ src/sql/election/proposal.sqlite.sql
@@ -1,7 +1,7 @@
 CREATE TABLE IF NOT EXISTS "Proposals" (
   "refuser" TEXT NOT NULL,
   "refpost" TEXT NOT NULL,
-  "refelection" TEXT NOT NULL,
+  "refelection" INTEGER NOT NULL,
   FOREIGN KEY("refuser") REFERENCES "Users"("username"),
   FOREIGN KEY("refpost") REFERENCES "Posts"("postname"),
   FOREIGN KEY("refelection") REFERENCES "Elections"("id"),
