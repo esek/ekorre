780635a08b71e53522663a6dc6135715237cd7e8
Emil Jonathan Eriksson <eje1999@gmail.com>
2021-11-23 14:09:48 +0100

    `NominationResponse` är nu enum
    Insåg att det var en väldigt dålig idé att ha detta som bool, då JS gör
    rätt whacky translations mellan null (vilket skulle varit `NoAnswer` och
    `false` (no))

--- src/sql/election/nomination.sqlite.sql
+++ src/sql/election/nomination.sqlite.sql
@@ -2,7 +2,7 @@ CREATE TABLE IF NOT EXISTS "Nominations" (
   "refuser" TEXT NOT NULL,
   "refpost" TEXT NOT NULL,
   "refelection" INT NOT NULL,
-  "accepted" BOOLEAN DEFAULT NULL,
+  "accepted" TEXT NOT NULL, -- TS Enum
   FOREIGN KEY("refuser") REFERENCES "Users"("username"),
   FOREIGN KEY("refpost") REFERENCES "Posts"("postname"),
   FOREIGN KEY("refelection") REFERENCES "Elections"("id"),
