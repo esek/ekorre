e6add4946d5f7bec94dbe8a91e10124ec1381ecc
Emil Jonathan Eriksson <eje1999@gmail.com>
2021-12-14 11:28:45 +0100

    Ökar timeout SQLite

--- src/api/knex.ts
+++ src/api/knex.ts
@@ -13,7 +13,10 @@ if (process.env.DB_CLIENT === 'sqlite') {
   // Gör att vi får foreign keys i sqlite. Eftersom detta
   // Promise kommer lösas före alla queries på `knex`-objektet
   // är det lugnt att vi inte använder top-level `await` här
-  k.raw('PRAGMA foreign_keys = ON;').then();
+  //
+  // Vi ökar även timeout, då våra tester vill göra väldigt mycket
+  // parallellt (vilket sqlite INTE gillar). Default är 30000.
+  k.raw('PRAGMA foreign_keys = ON; PRAGMA busy_timeout = 1200000;').then();
 } else {
   k = Knex({
     client: process.env.DB_CLIENT ?? 'mysql2',
