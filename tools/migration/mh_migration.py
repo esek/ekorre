7e88dd03ba014b31b5cc6aa1e10a07033abb9469
Emil Jonathan Eriksson <eje1999@gmail.com>
2021-12-29 17:01:54 +0100

    File upload works

--- tools/migration/mh_migration.py
+++ tools/migration/mh_migration.py
@@ -1,3 +1,5 @@
+#!/usr/bin/env python3
+
 """
 Author: Emil Eriksson (E18)
 Usage: mh_migration.py <moteshandlingar dir> <API URL>
@@ -39,6 +41,7 @@ import sys
 import re
 import getpass
 import requests as req
+from http.cookies import BaseCookie
 
 """
 {
@@ -60,11 +63,11 @@ meeting_docs = {}
 def append_file(filename: str, year: str) -> None:
     # Hitta typ av möte med regex
     try:
-      meeting_type_match = re.search(
-      r"(ht|s|vm|vt|smextra)(?:\d{1,2})?-.*\.pdf", filename).group(1)
+        meeting_type_match = re.search(
+            r"(ht|s|vm|vt|smextra)(?:\d{1,2})?-.*\.pdf", filename).group(1)
     except:
-      print(f"AAAH okänd mötestyp för år {year}: {filename}!")
-      sys.exit(1)
+        print(f"AAAH okänd mötestyp för år {year}: {filename}!")
+        sys.exit(1)
 
     if meeting_type_match == "ht":
         meeting_type = "HTM"
@@ -72,11 +75,12 @@ def append_file(filename: str, year: str) -> None:
     elif meeting_type_match == "s":
         meeting_type = "SM"
         try:
-          number = int(
-              re.search(r"s(\d+)(?:-\d+)?-.*\.pdf", filename).group(1))
+            number = int(
+                re.search(r"s(\d+)(?:-\d+)?-.*\.pdf", filename).group(1))
         except AttributeError:
-          print(f"AAAAH kunde inte hitta vilket styrelsemöte följande var: {filename}")
-          sys.exit(1)
+            print(
+                f"AAAAH kunde inte hitta vilket styrelsemöte följande var: {filename}")
+            sys.exit(1)
     elif meeting_type_match == "vm":
         meeting_type = "VM"
         number = -1
@@ -86,37 +90,38 @@ def append_file(filename: str, year: str) -> None:
     elif meeting_type_match == "smextra":
         meeting_type = "Extra"
         try:
-          number = int(
-              re.search(r"smextra(\d{1,2})(?:-\d{,2})?-.*\.pdf", filename).group(1))
+            number = int(
+                re.search(r"smextra(\d{1,2})(?:-\d{,2})?-.*\.pdf", filename).group(1))
         except AttributeError:
-          print(f"AAAAH kunde inte hitta vilket extramöte följande var: {filename}")
-          sys.exit(1)
+            print(
+                f"AAAAH kunde inte hitta vilket extramöte följande var: {filename}")
+            sys.exit(1)
 
     document_type_match = re.search(
         r".*-(kallelse|handlingar|sena handlingar|protokoll|bilaga)", filename).group(1)
 
     if document_type_match == "kallelse":
-      document_type = "summons"
+        document_type = "summons"
     elif document_type_match == "handlingar":
-      document_type = "documents"
+        document_type = "documents"
     elif document_type_match == "sena handlingar":
-      document_type = "lateDocuments"
+        document_type = "lateDocuments"
     elif document_type_match == "protokoll":
-      document_type = "protocol"
+        document_type = "protocol"
     elif document_type_match == "bilaga":
-      document_type = "appendix"
+        document_type = "appendix"
     else:
-      print(f"AAAAH okänd dokumenttyp för år {year}: {filename}")
-      sys.exit(1)
+        print(f"AAAAH okänd dokumenttyp för år {year}: {filename}")
+        sys.exit(1)
 
     if year not in meeting_docs.keys():
         meeting_docs[year] = []
-    
+
     meeting_docs[year].append({
-      "meeting_type": meeting_type,
-      "document_type": document_type,
-      "number": number,
-      "filename": filename,
+        "meeting_type": meeting_type,
+        "document_type": document_type,
+        "number": number,
+        "filename": filename,
     })
 
 
@@ -136,17 +141,23 @@ def migrate_to_ekorre():
     username = "aa0000bb-s"
     password = "test"
 
-    session = req.Session()
-    res = session.post(base_api_url, json={
-      "query": LOGIN_QUERY,
-      "variables": {
-        "username": username,
-        "password": password
-      }
+    res = req.post(base_api_url, json={
+        "query": LOGIN_QUERY,
+        "variables": {
+            "username": username,
+            "password": password
+        }
     })
 
-    print(res.text)
-    print(session.cookies)
+    # requests leker inte snällt med localhost...
+    # Så vi måste ladda in kakors rådata, och sen skapa
+    # nya som gäller för alla domäner
+    cookie_jar = req.cookies.RequestsCookieJar()
+    bc = BaseCookie()
+    bc.load(res.headers['Set-Cookie'])
+
+    for cookie in bc.items():
+        cookie_jar.set(cookie[1].key, cookie[1].value)
 
     GET_MEETING_ID_QUERY = """
       {
@@ -169,20 +180,19 @@ def migrate_to_ekorre():
     """
 
     for year in meeting_docs.keys():
-      print(f"Uploading files for year {year}")
-      for meeting_doc in meeting_docs[year]:
-        with open(meeting_doc['filename'], 'rb') as f:
-          data = {
-            "body": {
-              "path": f"/moteshandlingar/{year}/"
-            },
-            "files": {
-              "file": f.read()
+        print(f"Uploading files for year {year}")
+        for meeting_doc in meeting_docs[year]:
+            data = {
+                "body": {
+                    "path": f"/moteshandlingar/{year}/"
+                },
             }
-          }
-        file_res = session.post(f"{base_api_url}/files/upload", data=data)
-        print(file_res.text)
-        sys.exit(0)
+            with open(meeting_doc['filename'], 'rb') as f:
+                file_res = req.post(
+                    f"{base_api_url}/files/upload", data=data, files={'file': f.read()}, cookies=cookie_jar)
+            print(file_res.text)
+            sys.exit(0)
+
 
 if __name__ == "__main__":
     # Vi antar att första argumentet passed är ett dir med alla år (`moteshandlingar`)
@@ -197,5 +207,5 @@ if __name__ == "__main__":
         print(f"Parsing filenames for {year_dir}")
         for filename in os.listdir(os.path.join(root_dir, year_dir)):
             append_file(os.path.join(root_dir, year_dir, filename), year_dir)
-    
+
     migrate_to_ekorre()
