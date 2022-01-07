045cd2dd9e1c2d93b976ccdbe5c94f65bbca1de1
Emil Jonathan Eriksson <eje1999@gmail.com>
2022-01-07 22:31:59 +0100

    Lägger till HeHE i resolvers/index.ts
    Visste inte att detta behövde göras, speciellt inte manuellt...

--- src/resolvers/index.ts
+++ src/resolvers/index.ts
@@ -7,6 +7,7 @@ export { default as auth } from './auth.resolver';
 export { default as email } from './email.resolver';
 export { default as emergencycontact } from './emergencycontact.resolver';
 export { default as file } from './file.resolver';
+export { default as hehe } from './hehe.resolver';
 export { default as meeting } from './meeting.resolver';
 export { default as post } from './post.resolver';
 export { default as election } from './election.resolver';
