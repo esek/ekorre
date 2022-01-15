7e1bdf2acf54701ebfbdc92c1a43ddc043833fd7
Emil Jonathan Eriksson <eje1999@gmail.com>
2022-01-15 17:11:28 +0100

    Fix dir copying

--- initenv.sh
+++ initenv.sh
@@ -10,5 +10,13 @@ cp .env.example.dev .env
 sed -i'' -e "s:DB_FILE=.*:DB_FILE=${DATABASE_PATH}:g" .env # Undvik / i regex då paths innehåller det, -i'' för POSIX compliance
 
 echo "Fixar public mapp"
-cp -r public/ $FILES_PATH
+
+# Antingen finns hela directoriet, och då kopierar vi in filerna (public/),
+# eller så kopierar vi även public
+if [ -d $FILES_PATH ]; then
+  cp -r public/ $FILES_PATH
+else
+  cp -r public $FILES_PATH
+fi
+
 sed -i'' -e "s:FILE_ROOT=.*:FILE_ROOT=${FILES_PATH}:g" .env
