a2bd0f26edc119d374645e33c0aace38a1bbcb29
Emil Jonathan Eriksson <eje1999@gmail.com>
2022-01-17 21:48:51 +0100

    `app.listen()` call moved to facilitate testing
    Having this call in `src/app.ts` caused issues, as this handle did not
    end on tests ending. This caused errors such as port marked as in-use,
    among other things. This should speed up testing without any cost.

---
+++ src/index.ts
@@ -0,0 +1,14 @@
+import { app } from './app';
+import config from './config';
+import { Logger } from './logger';
+
+const { PORT, HOST } = config;
+
+Logger.logLevel = Logger.getLogLevelFromString(process.env.LOGLEVEL ?? 'normal');
+const logger = Logger.getLogger('App');
+
+logger.log('Beginning startup...');
+
+app.listen(PORT, HOST, () => {
+  logger.log(`Server started on http://${HOST}:${PORT}`);
+});
