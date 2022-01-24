3d400432721915fcc4f2f468743978342867ff27
Axel Froborg <23397446+afroborg@users.noreply.github.com>
2022-01-24 15:54:13 +0100

    conditionally run perl/sed

--- initenv.sh
+++ initenv.sh
@@ -7,8 +7,16 @@ FILES_PATH=$PWD/public.local
 echo "Skapar en databas under namnet $DATABASE_PATH"
 sqlite3 -init src/sql/init.sql $DATABASE_PATH .exit
 cp .env.example.dev .env
-perl -i -pe"s:DB_FILE=.*:DB_FILE=${DATABASE_PATH}:g" .env # Undvik / i regex då paths innehåller det, -i'' för POSIX compliance
 
+function replace() {
+  if [[ "$OSTYPE" == "darwin"* ]]; then
+    perl -i -pe$1 $2
+  else
+    sed -i'' -e$1 $2
+  fi 
+}
+
+replace "s:DB_FILE=.*:DB_FILE=${DATABASE_PATH}:g" .env # Undvik / i regex då paths innehåller det, -i'' för POSIX compliance
 echo "Fixar public mapp"
 
 # Antingen finns hela directoriet, och då kopierar vi in filerna (public/),
@@ -19,4 +27,4 @@ else
   cp -r public $FILES_PATH
 fi
 
-perl -i -pe"s:FILE_ROOT=.*:FILE_ROOT=${FILES_PATH}:g" .env
+replace 's:FILE_ROOT=.*:FILE_ROOT='$FILES_PATH':' .env
