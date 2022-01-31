9e77a9aaf9c8baf2c3374552a6aff2ad6359f700
Emil Jonathan Eriksson <eje1999@gmail.com>
2022-01-31 15:31:10 +0100

    Cleanup, add command-exists check

--- tools/benchmark/index.ts
+++ tools/benchmark/index.ts
@@ -1,10 +1,19 @@
-import { exec } from 'child_process';
+#!/usr/bin/env node
+// This is only ever to be run in development environment,
+// so requiring depencencies in production is not needed
+/* eslint-disable import/no-extraneous-dependencies */
+
+import { exec, execSync } from 'child_process';
+import { platform } from 'os';
 import fs from 'fs';
 import inquirer from 'inquirer';
+import yargs from 'yargs';
+import commandExists = require('command-exists');
 import path from 'path';
+import 'dotenv/config';
 
 const SCRIPTS_FOLDER = path.join(__dirname, './scripts');
-const PORT = 3001;
+const { PORT } = process.env;
 
 const selectScript = async () => {
   const scripts = fs.readdirSync(SCRIPTS_FOLDER);
@@ -19,6 +28,13 @@ const selectScript = async () => {
 };
 
 const run = async () => {
+  try {
+    await commandExists('wrk');
+  } catch (err) {
+    console.error('wrk måste finnas installerat för att kunna köra benchmarking,\nse https://github.com/wg/wrk');
+    process.exit(1);
+  }
+
   const script = await selectScript();
 
   if (!script) {
