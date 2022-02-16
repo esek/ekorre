9c63f2c736a57a9af3be9d20952eeb126e787355
Emil Jonathan Eriksson <eje1999@gmail.com>
2022-02-16 22:08:58 +0100

    Removed now unused Database types

--- src/models/db/article.d.ts
+++
@@ -1,6 +0,0 @@
-import type { Article } from '@generated/graphql';
-
-export type DatabaseArticle = Omit<Article, 'creator' | 'lastUpdatedBy' | 'slug'> & {
-  refcreator: string;
-  reflastupdateby: string;
-};
--- src/models/db/emergencycontact.d.ts
+++
@@ -1,5 +0,0 @@
-import { EmergencyContact } from '@generated/graphql';
-
-export type DatabaseEmergencyContact = EmergencyContact & {
-  refuser: string;
-};
