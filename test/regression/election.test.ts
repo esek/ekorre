2323ba2a154baad96451a46034ebb972275cba6a
Emil Jonathan Eriksson <eje1999@gmail.com>
2022-03-24 20:34:08 +0100

    Working with updating tests for prisma

--- test/regression/election.test.ts
+++ test/regression/election.test.ts
@@ -1,16 +1,11 @@
-import { ELECTABLE_TABLE, ELECTION_TABLE, NOMINATION_TABLE, PROPOSAL_TABLE } from '@/api/constants';
-import db from '@/api/knex';
 import { ElectionAPI } from '@api/election';
-import {
-  DatabaseElectable,
-  DatabaseElection,
-  DatabaseNomination,
-  DatabaseProposal,
-} from '@db/election';
-import { Election, NominationResponse } from '@generated/graphql';
+import { Election, NominationAnswer } from '@generated/graphql';
+import { PrismaClient } from '@prisma/client';
 import { ApiRequest, GraphqlResponse } from '@test/models/test';
 import { AXIOS_CONFIG } from '@test/utils/axiosConfig';
 import axios from 'axios';
+
+const prisma = new PrismaClient();
 
 const api = new ElectionAPI();
 interface ElectionResponse {
@@ -34,37 +29,16 @@ const ELECTION_QUERY = `
 }
 `;
 
-let preTestElectionTable: DatabaseElection[];
-let preTestElectableTable: DatabaseElectable[];
-let preTestProposalTable: DatabaseProposal[];
-let preTestNominationTable: DatabaseNomination[];
-
 const clearDatabase = async () => {
   // Vi sätter `where` till något som alltid är sant
-  await db<DatabaseElectable>(ELECTABLE_TABLE).delete().whereNotNull('refelection');
-  await db<DatabaseProposal>(PROPOSAL_TABLE).delete().whereNotNull('refelection');
-  await db<DatabaseNomination>(NOMINATION_TABLE).delete().whereNotNull('refelection');
-  await db<DatabaseElection>(ELECTION_TABLE).delete().whereNotNull('id');
+  await prisma.prismaElectable.deleteMany();
+  await prisma.prismaProposal.deleteMany();
+  await prisma.prismaNomination.deleteMany();
+  await prisma.prismaElection.deleteMany();
 };
-
-beforeAll(async () => {
-  preTestElectionTable = await db<DatabaseElection>(ELECTION_TABLE).select('*');
-  preTestElectableTable = await db<DatabaseElectable>(ELECTABLE_TABLE).select('*');
-  preTestProposalTable = await db<DatabaseProposal>(PROPOSAL_TABLE).select('*');
-  preTestNominationTable = await db<DatabaseNomination>(NOMINATION_TABLE).select('*');
-  await clearDatabase();
-});
 
 afterEach(async () => {
   await clearDatabase();
-});
-
-afterAll(async () => {
-  // Sätt in cachade värden igen
-  await db<DatabaseElection>(ELECTION_TABLE).insert(preTestElectionTable);
-  await db<DatabaseElectable>(ELECTABLE_TABLE).insert(preTestElectableTable);
-  await db<DatabaseProposal>(PROPOSAL_TABLE).insert(preTestProposalTable);
-  await db<DatabaseNomination>(NOMINATION_TABLE).insert(preTestNominationTable);
 });
 
 test('getting nominations when nominations are hidden', async () => {
@@ -73,9 +47,9 @@ test('getting nominations when nominations are hidden', async () => {
   await expect(api.nominate('aa0000bb-s', ['Macapär'])).resolves.toBeTruthy();
   await expect(api.nominate('bb1111cc-s', ['Macapär', 'Teknokrat'])).resolves.toBeTruthy();
   await expect(
-    api.respondToNomination('aa0000bb-s', 'Macapär', NominationResponse.Yes),
+    api.respondToNomination('aa0000bb-s', 'Macapär', NominationAnswer.Yes),
   ).resolves.toBeTruthy();
-  expect((await api.getAllNominations(electionId, NominationResponse.Yes)).length).toBeGreaterThan(0);
+  expect((await api.getAllNominations(electionId, NominationAnswer.Yes)).length).toBeGreaterThan(0);
 
   const electionData = {
     query: ELECTION_QUERY,
@@ -109,6 +83,6 @@ test('getting nominations when nominations are hidden', async () => {
 
       // Borde bara se accepterade nomineringen
       expect(acceptedNominations).toHaveLength(1);
-      expect(acceptedNominations[0].accepted).toEqual(NominationResponse.Yes);
+      expect(acceptedNominations[0].accepted).toEqual(NominationAnswer.Yes);
     });
 });
