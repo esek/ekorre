1dd8fa62ab8f52f39ed998b8f80d28117920c670
Emil Blennow <emil.blennow99@gmail.com>
2022-04-01 22:11:57 +0200

    Util för access och onDelete: Cascade saker

--- test/utils/utils.ts
+++ test/utils/utils.ts
@@ -1,3 +1,8 @@
+import { AccessAPI } from '@api/access';
+import { ApiKeyAPI } from '@api/apikey';
+import { UserAPI } from '@api/user';
+import { AccessInput, NewUser } from '@generated/graphql';
+
 /**
  * Extrahera en token ur en set-cookie-sträng, eller returnera
  * `null` om ingen hittas
@@ -15,4 +20,40 @@ export const extractToken = (tokenName: string, s?: string): string | null => {
     return match[0];
   }
   return null;
+};
+
+type NOOP<T = void> = () => Promise<T>;
+const userApi = new UserAPI();
+const apiKeyApi = new ApiKeyAPI();
+const accessApi = new AccessAPI();
+
+export const genUserWithAccess = (userInfo: NewUser, access: AccessInput): [NOOP, NOOP] => {
+  const create = async () => {
+    await userApi.createUser(userInfo);
+    await accessApi.setIndividualAccess(userInfo.username, access);
+  };
+
+  const remove = async () => {
+    await userApi.clear();
+  };
+
+  return [create, remove];
+};
+
+export const genApiKey = (userInfo: NewUser, access: AccessInput): [NOOP<string>, NOOP] => {
+  let apikey: string;
+  const create = async () => {
+    await userApi.createUser(userInfo);
+    apikey = await apiKeyApi.createApiKey('Test API key', userInfo.username);
+    await accessApi.setApiKeyAccess(apikey, access);
+
+    return apikey;
+  };
+
+  const remove = async () => {
+    await apiKeyApi.removeApiKey(apikey);
+    await userApi.clear();
+  };
+
+  return [create, remove];
 };
