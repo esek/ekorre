b7220f307bbd85ab981cf5bf55816e2fe5011acd
Emil Jonathan Eriksson <eje1999@gmail.com>
2022-05-12 20:43:11 +0200

    User dataloader tests passing

--- test/unit/user.dataloader.test.ts
+++ test/unit/user.dataloader.test.ts
@@ -1,46 +1,18 @@
-import { USER_TABLE } from '@/api/constants';
-import db from '@/api/knex';
 import { createDataLoader } from '@/dataloaders';
 import { batchUsersFunction, userApi } from '@/dataloaders/user.dataloader';
 import { NotFoundError } from '@/errors/request.errors';
-import { DatabaseUser } from '@/models/db/user';
+import { PrismaUser } from '@prisma/client';
 import { reduce } from '@/reducers';
 import { userReduce } from '@/reducers/user.reducer';
+import { genRandomUser } from '@test/utils/utils';
+
+const [createUser0, deleteUser0] = genRandomUser([]);
+const [createUser1, deleteUser1] = genRandomUser([]);
+const [createUser2, deleteUser2] = genRandomUser([]);
 
 // Vi kontrollerar antal anrop till API:n
 const apiSpy = jest.spyOn(userApi, 'getMultipleUsers');
-const mockUsers: DatabaseUser[] = [
-  {
-    username: 'dataloaderTestUser1',
-    passwordHash: 'hhhhhhhh',
-    passwordSalt: 'duvetvad',
-    firstName: 'Jalle',
-    lastName: 'Testsson',
-    email: 'no-reply@esek.se',
-    class: 'E18',
-    isFuncUser: false,
-  },
-  {
-    username: 'dataloaderTestUser2',
-    passwordHash: 'kkkkkkkk',
-    passwordSalt: 'duvetvad',
-    firstName: 'Balle',
-    lastName: 'Testsson',
-    email: 'no-reply@esek.se',
-    class: 'C18',
-    isFuncUser: false,
-  },
-  {
-    username: 'dataloaderTestUser0',
-    passwordHash: 'ddddddddddddddddddddddd',
-    passwordSalt: 'duvetvad', // E-votes salt för samtliga valkoder före 2021
-    firstName: 'Falle',
-    lastName: 'Testsson',
-    email: 'no-reply@esek.se',
-    class: 'E18',
-    isFuncUser: false,
-  },
-];
+let mockUsers: PrismaUser[] = [];
 
 beforeEach(() => {
   // För att vi ska återställa räkningen
@@ -50,27 +22,28 @@ beforeEach(() => {
 
 beforeAll(async () => {
   // Insert fake users
-  await db<DatabaseUser>(USER_TABLE).insert(mockUsers);
+  mockUsers = await Promise.all([
+    createUser0(),
+    createUser1(),
+    createUser2(),
+  ]);
 });
 
 afterAll(async () => {
-  await db<DatabaseUser>(USER_TABLE)
-    .delete()
-    .whereIn(
-      'username',
-      mockUsers.map((u) => u.username),
-    );
+  await Promise.all([
+    deleteUser0(),
+    deleteUser1(),
+    deleteUser2(),
+  ]);
 });
 
 test('load single user', async () => {
+  const mockUser0 = reduce(mockUsers[0], userReduce);
+
   const dl = createDataLoader(batchUsersFunction);
-  const user = await dl.load('dataloaderTestUser0');
-  user.isFuncUser = !!user.isFuncUser;
-  const mockUser = reduce(
-    mockUsers.filter((u) => u.username === 'dataloaderTestUser0')[0],
-    userReduce,
-  );
-  expect(user).toMatchObject(mockUser);
+  const user = await dl.load(mockUser0.username);
+  
+  expect(user).toMatchObject(mockUser0);
   expect(apiSpy).toHaveBeenCalledTimes(1);
 });
 
@@ -79,10 +52,6 @@ test('load multiple users', async () => {
   await Promise.all(
     mockUsers.map(async (u) => {
       const user = await dl.load(u.username);
-      // SQLite converts false to 0, true to 1,
-      // this reconverts
-      user.isFuncUser = !!user.isFuncUser;
-
       const mockUser = reduce(u, userReduce);
       expect(user).toMatchObject(mockUser);
     }),
@@ -99,10 +68,6 @@ test('loading multiple existant and non-existant users', async () => {
       // expect the load of that name to be that user
       if (mockUsers.map((u) => u.username).includes(name)) {
         const user = await dl.load(name);
-
-        // SQLite converts false to 0, true to 1,
-        // this reconverts
-        user.isFuncUser = !!user.isFuncUser;
 
         const mockUser = reduce(mockUsers.filter((u) => u.username === name)[0], userReduce);
 
