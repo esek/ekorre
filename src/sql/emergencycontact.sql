2599962f332632d193098c7fd8de454db7734b9d
Emil Jonathan Eriksson <eje1999@gmail.com>
2022-05-17 14:01:02 +0200

    Remove SQL files

--- src/sql/emergencycontact.sql
+++
@@ -1,8 +0,0 @@
-CREATE TABLE IF NOT EXISTS "EmergencyContacts" (
-  "id" INTEGER PRIMARY KEY AUTOINCREMENT,
-  "name" TEXT NOT NULL,
-  "type" TEXT NOT NULL,
-  "phone" TEXT NOT NULL,
-	"refuser" TEXT NOT NULL,
-  FOREIGN KEY("refuser") REFERENCES "Users"("username")
-);
--- src/sql/hehe.sqlite.sql
+++
@@ -1,9 +0,0 @@
-CREATE TABLE IF NOT EXISTS "Hehes" (
-  "number" INT NOT NULL,
-  "year" INT NOT NULL,
-  "refuploader" TEXT NOT NULL,
-  "reffile" TEXT NOT NULL,
-  FOREIGN KEY("refuploader") REFERENCES "Users"("username"),
-  FOREIGN KEY("reffile") REFERENCES "Files"("id"),
-  PRIMARY KEY ("number", "year")
-);
