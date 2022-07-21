fcfdbca2a6a1b39feab91e57a3df61efec3147e6
Axel Froborg <23397446+afroborg@users.noreply.github.com>
2022-07-22 00:06:00 +0200

    fix seed command

--- prisma/data/access.seed.ts
+++ prisma/data/access.seed.ts
@@ -15,3 +15,19 @@ export const postAccess: Prisma.PrismaPostAccessUncheckedCreateInput[] = [
     resourceType: PrismaResourceType.feature,
   },
 ];
+
+export const apiKeys: Prisma.PrismaApiKeyUncheckedCreateInput[] = [
+  {
+    refCreator: 'aa0000bb-s',
+    description: 'Seeding api key (dont remove pls)',
+    key: 'af0bba60-6efc-4026-a484-f212ce4a5843',
+  },
+];
+
+export const apiKeyAccess: Prisma.PrismaApiKeyAccessUncheckedCreateInput[] = [
+  {
+    refApiKey: apiKeys[0].key,
+    resource: 'superadmin',
+    resourceType: PrismaResourceType.feature,
+  },
+];
--- prisma/data/seed.ts
+++ prisma/data/seed.ts
@@ -1,6 +1,6 @@
 import { PrismaClient } from '@prisma/client';
 
-import { individualAccess, postAccess } from './access.seed';
+import { apiKeyAccess, apiKeys, individualAccess, postAccess } from './access.seed';
 import { posts } from './post.seed';
 import { users } from './user.seed';
 
@@ -11,9 +11,13 @@ const run = async () => {
     prisma.prismaUser.createMany({ data: users, skipDuplicates: true }),
     prisma.prismaPost.createMany({ data: posts, skipDuplicates: true }),
   ]);
+
+  await prisma.prismaApiKey.createMany({ data: apiKeys, skipDuplicates: true });
+
   await Promise.all([
     prisma.prismaIndividualAccess.createMany({ data: individualAccess, skipDuplicates: true }),
     prisma.prismaPostAccess.createMany({ data: postAccess, skipDuplicates: true }),
+    prisma.prismaApiKeyAccess.createMany({ data: apiKeyAccess, skipDuplicates: true }),
   ]);
 };
 
