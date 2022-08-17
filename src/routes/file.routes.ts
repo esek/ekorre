941ba598f421989c3b37e218de16c789525b79c0
Emil Jonathan Eriksson <eje1999@gmail.com>
2022-08-17 15:02:19 +0200

    Use constants once more

--- src/routes/file.routes.ts
+++ src/routes/file.routes.ts
@@ -58,7 +58,7 @@ filesRoute.post('/upload', upload(), verifyAuthenticated, async (req, res) => {
   if (file.size > maxSize) {
     return res
       .status(400)
-      .send(`File too big, max is ${config.FILES.MAX_FILE_UPLOAD_SIZE_BYTES / BYTES_PER_MB} MB`);
+      .send(`File too big, max is ${maxSize / BYTES_PER_MB} MB`);
   }
 
   const accessType = body?.accessType ?? AccessType.Public;
@@ -84,7 +84,11 @@ filesRoute.post('/upload/avatar', upload(), verifyAuthenticated, async (req, res
   if (file.size > maxAvatarSize || file.size > maxSize) {
     return res
       .status(400)
-      .send(`Avatar too big, max is ${config.FILES.MAX_AVATAR_SIZE_BYTES / BYTES_PER_MB} MB`);
+      .send(
+        `Avatar too big, max is ${
+          (maxAvatarSize > maxSize ? maxSize : maxAvatarSize) / BYTES_PER_MB
+        } MB`,
+      );
   }
 
   const { user } = res.locals;
